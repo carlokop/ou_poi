818248a83a999b37b151aa1f9e38de07d7fb0771
carlokop <ch.kop.ck@gmail.com>
2024-06-03 19:42:55 +0200

    creatie klant

--- Domein/Klant.java
+++ Domein/Klant.java
@@ -1,5 +1,66 @@
 package Domein;
 
-public class Klant {
-	Postcode postcodeInfo;
+public class Klant  {
+	private Postcode postcode;
+	private int klantnr;
+	
+	/**
+	 * Maakt nieuwe klant
+	 * @param klantnr          het klantnummer
+	 * @param postcodeInfo     postcodeinfo object
+	 * @throws IllegalArgumentException als foutive waarde wordt meegegeven
+	 * 
+	 * @contract happy
+	 *  @requires klantnr > 0
+	 *  @requires postcodeinfo != null
+	 *  @ensures /result is een nieuwe klant
+	 *  @signals IllegalArgumentException als klantnr of postcodeinfo ongeldig zijn
+	 * @contract postcode null
+	 *   @requires postcode == null
+	 *   @signals IllegalArgumentException("Postcode mag niet null zijn")
+	 * @contract klantnummer onjuist
+	 *   @requires klantnummer <= 0
+	 *   @signals IllegalArgumentException("Klantknummer moet een positief getal zijn")
+	 *  
+	 */
+	Klant(int klantnr, Postcode postcode) throws IllegalArgumentException {
+	  //test klantnummer positief > 0
+	  if(klantnr <= 0) {
+	    throw new IllegalArgumentException("Klantknummer moet een positief getal zijn");
+	  }
+	  
+	  //test null postcode
+	  if(postcode == null) {
+	    throw new IllegalArgumentException("Postcode mag niet null zijn");
+	  }
+	  
+	  this.klantnr = klantnr;
+	  this.postcode = postcode;
+	  
+	}
+	
+	/**
+	 * Geeft het klantnummer
+	 * @return klantnr
+	 */
+	public int getKlantnr() {
+	  return klantnr;
+	}
+	
+//	@Override
+//	public Klant clone(Object klant) throws CloneNotSupportedException {
+//	  
+//	  
+//	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
